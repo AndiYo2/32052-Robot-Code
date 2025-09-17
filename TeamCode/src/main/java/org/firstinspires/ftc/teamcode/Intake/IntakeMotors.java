dd7c05a0c9ff3c8fcb4b0e3a665a0ab151f1f8e2
AndiYo2 <andyi2@icloud.com>
2025-09-17 18:58:06 -0500

    Meet the Timberwolves bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/IntakeMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake/IntakeMotors.java
@@ -10,10 +10,8 @@ public class IntakeMotors {
 
     // Declare variables for the motor
     double speed = .75;
-    double shootTime = 5;
-    boolean running = false;
+    //boolean running = false;
     private DcMotor intakeMotor;
-    Timer timer = new Timer();
 
 
 
@@ -26,19 +24,7 @@ public class IntakeMotors {
         intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
-    public void grabBall(){
-        intakeMotor.setPower(speed);
-        System.out.println("Timer Started");
-        timer.schedule(new StopMotor(intakeMotor), 5000);
-    }
-    public void toggleIntake(){
-        running = !running;
-        if(running){
-            intakeMotor.setPower(speed);
-        }else{
-            intakeMotor.setPower(0);
-        }
-    }
+
     public void intakeBall(){
         intakeMotor.setPower(speed);
     }
@@ -46,28 +32,13 @@ public class IntakeMotors {
         intakeMotor.setPower(0);
     }
 
-   /* public boolean checkBall(){
-        //if there is a ball, then we categorize it and do stuff
-        return false;
-    }
-
-    public void changeIntakeStatus(){
+    /*public void toggleIntake(){
         running = !running;
-    }*/
-
-    static class StopMotor extends TimerTask{
-        DcMotor motor;
-
-        StopMotor(DcMotor motor){
-            this.motor = motor;
-        }
-
-        @Override
-        public void run() {
-            motor.setPower(0);
-            System.out.println("Timer has been completed");
+        if(running){
+            intakeMotor.setPower(speed);
+        }else{
+            intakeMotor.setPower(0);
         }
     }
-
-
+    */
 }
